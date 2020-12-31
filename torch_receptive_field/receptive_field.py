8ba4b98dce6003ce60da68ace2b8ce4710cf93f8
Yonghye Kwon <developer.0hye@gmail.com>
2020-12-31 13:57:30 +0900

    support dilated convolution
    support dilated convolution

--- torch_receptive_field/receptive_field.py
+++ torch_receptive_field/receptive_field.py
@@ -47,9 +47,11 @@ def receptive_field(model, input_size, batch_size=-1, device="cuda"):
                     kernel_size = module.kernel_size
                     stride = module.stride
                     padding = module.padding
-                    kernel_size, stride, padding = map(check_same, [kernel_size, stride, padding])
+                    dilation = module.dilation
+       
+                    kernel_size, stride, padding, dilation = map(check_same, [kernel_size, stride, padding, dilation])
                     receptive_field[m_key]["j"] = p_j * stride
-                    receptive_field[m_key]["r"] = p_r + (kernel_size - 1) * p_j
+                    receptive_field[m_key]["r"] = p_r + ((kernel_size - 1) * dilation) * p_j
                     receptive_field[m_key]["start"] = p_start + ((kernel_size - 1) / 2 - padding) * p_j
                 elif class_name == "BatchNorm2d" or class_name == "ReLU" or class_name == "Bottleneck":
                     receptive_field[m_key]["j"] = p_j
