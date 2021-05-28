ca05a64ca56d47b90d30e3a9582e253bb0659178
Ahmed Shahin <ahmedhassan9922@gmail.com>
2021-05-28 14:54:52 +0200

    Support and Testing for 3D CNNs

--- main.py
+++ main.py
@@ -18,9 +18,27 @@ class Net(nn.Module):
         y = self.maxpool(y)
         return y
 
+class Net3D(nn.Module):
+    def __init__(self):
+        super(Net3D, self).__init__()
+        self.conv = nn.Conv3d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn = nn.BatchNorm3d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
+
+    def forward(self, x):
+        y = self.conv(x)
+        y = self.bn(y)
+        y = self.relu(y)
+        y = self.maxpool(y)
+        return y
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
 model = Net().to(device)
 
 receptive_field_dict = receptive_field(model, (3, 256, 256))
+receptive_field_for_unit(receptive_field_dict, "2", (1,1))
+
+model = Net3D().to(device)
+receptive_field_dict = receptive_field(model, (3, 256, 256, 256))
 receptive_field_for_unit(receptive_field_dict, "2", (1,1))
