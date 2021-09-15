27535706a02a6952c8759a3856abb98719179f89
JackLangerman <JackMLangerman@gmail.com>
2021-09-15 19:09:31 +0000

    make pip installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='torch_receptive_field',
+	version='0.0.1',
+	description='Compute CNN receptive field size in pytorch in one line',
+	url='https://github.com/Fangyh09/pytorch-receptive-field',
+	author='Fangyh09',
+	packages=['torch_receptive_field'],
+        )
